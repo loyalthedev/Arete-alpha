a49c230f4f35cd10dd901b8472f3e6af5f9213c0
Miles <webdevmiles@gmail.com>
2023-03-31 19:01:19 +0100

    initialising repo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  },
   experimental: {
     appDir: true,
+  },
+	devIndicators: {
+    buildActivity: false,
   },
 }
 

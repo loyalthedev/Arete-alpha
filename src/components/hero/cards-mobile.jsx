3317eacac76f797b1fc805397a86d07dc6d8244d
daddyloyal <lifeofemmany6@gmail.com>
2023-04-07 14:53:18 +0100

    Made some changes to the card mobile

--- src/components/hero/cards-mobile.jsx
+++ src/components/hero/cards-mobile.jsx
@@ -4,33 +4,33 @@ import './cards.style.css';
 
 export default function CardsMobile() {
   return (
-    <section className="flex flex-col relative mx-auto top-5 scale-[.6] lg:scale-100 h-96">
+    <section className="flex flex-wrap justify-center relative mx-auto bg-red-500 h-[16rem] top-5">
       {/* <Image src='/assets/linear-globe.svg' width={100} height={100} alt='linear Globe' className='relative top-56 left-36' /> */}
       {CARD_DATA.map((item, index) => (
         <div
-          className={`bg-black border-[1px] border-white rounded-[15px] p-4 relative cube ${
-            index == 0 ? 'z-[999]' : ''
-          } ${index == 1 ? 'z-[99] bottom-52' : ''} ${
-            index == 2 ? 'bottom-[26rem]' : ''
+          className={`bg-black border-[1px] border-white scale-[.70] rounded-[15px] p-4 relative cube ${
+            index == 0 ? 'z-[999] bottom-14' : ''
+          } ${index == 1 ? 'z-[99] bottom-[22rem]' : ''} ${
+            index == 2 ? 'bottom-[43rem]' : ''
           }`}
           key={item.id}
         >
           <Image
             src={item.image}
             alt="Display Image"
-            width={250}
-            height={250}
+            width={300}
+            height={300}
             className="mx-auto"
           />
-          <p className="text-white text-2xl uppercase my-3">{item.title}</p>
+          <p className="text-white text-base uppercase my-3">{item.title}</p>
           <div className="flex justify-between gap-3">
             <div className="price">
-              <p className="text-grey text-base">{item.subtitle}</p>
-              <p className="text-white text-xl">{item.price}</p>
+              <p className="text-grey text-xs">{item.subtitle}</p>
+              <p className="text-white text-sm">{item.price}</p>
             </div>
             <div className="time flex flex-col gap-1">
-              <p className="text-white text-xl">{item.digital}</p>
-              <p className="text-grey text-base relative bottom-2">{item.time}</p>
+              <p className="text-white text-sm">{item.digital}</p>
+              <p className="text-grey text-xs relative bottom-2">{item.time}</p>
             </div>
           </div>
         </div>
